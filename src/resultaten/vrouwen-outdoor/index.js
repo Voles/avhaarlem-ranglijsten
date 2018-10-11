a9de3feb27d3ae0aec4008b61ec351dd74e1ac95
Niels Dequeker <niels.dequeker@fastmail.com>
2018-10-11 19:59:00 +0200

    Add meisjes junioren d

--- src/resultaten/vrouwen-outdoor/index.js
+++ src/resultaten/vrouwen-outdoor/index.js
@@ -2,13 +2,15 @@ import senioren from './senioren';
 import juniorenA from './junioren-a';
 import juniorenB from './junioren-b';
 import juniorenC from './junioren-c';
+import juniorenD from './junioren-d';
 
 export default []
   .concat(
     senioren,
     juniorenA,
     juniorenB,
-    juniorenC
+    juniorenC,
+    juniorenD,
   )
   .map(resultaat => {
     resultaat.geslacht = 'Vrouwen';
