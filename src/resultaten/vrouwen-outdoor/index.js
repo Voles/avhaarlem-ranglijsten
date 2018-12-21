cac33bbaec4a326985c4738279c8ffd2f3160dce
Niels Dequeker <niels.dequeker@fastmail.com>
2018-12-21 19:43:42 +0100

    Move meisjes outdoor -> vrouwen outdoor

--- src/resultaten/vrouwen-outdoor/index.js
+++ src/resultaten/vrouwen-outdoor/index.js
@@ -1,8 +1,16 @@
 import senioren from './senioren';
+import juniorenA from './junioren-a';
+import juniorenB from './junioren-b';
+import juniorenC from './junioren-c';
+import juniorenD from './junioren-d';
 
 export default []
   .concat(
-    senioren
+    senioren,
+    juniorenA,
+    juniorenB,
+    juniorenC,
+    juniorenD,
   )
   .map(resultaat => {
     resultaat.geslacht = 'Vrouwen';
