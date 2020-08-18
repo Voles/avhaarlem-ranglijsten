a652318bbda2a313b8f597cc3b0ff971d6c3b705
Niels Dequeker <niels@newfront.eu>
2020-08-18 23:54:39 +0200

    Attempt to fix build

--- process-cms-data-to-js.sh
+++ process-cms-data-to-js.sh
@@ -6,8 +6,6 @@ set -u # fail on unknown variables
 FILES_TO_READ='./_data/clubrecords/*'
 DESTINATION='./src/resultaten/index.js'
 
-ls -d $FILES_TO_READ
-
 echo "const resultaten = " > $DESTINATION
 ls -d $FILES_TO_READ | xargs cat | jq -s . >> $DESTINATION
 echo "
